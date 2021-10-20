ec676b84696a4b4d2ca539ecee24dd13f0f6b8ce
Victoria Zavala <torizavala@aol.com>
2021-10-19 23:24:30 -0700

    1st commit - finished powerset helper function. now just to try running it with main powerset function

---
+++ powerset.py
@@ -0,0 +1,45 @@
+import copy
+
+def powerset(input):
+    """
+    When given a set of n distinct numbers, this function will return its powerset.
+    :param input: int
+    :return: int
+    """
+
+    # in progress
+
+def powerset_helper(pointer, choices_made, input, result):
+    """
+    Helper function for powerset function.
+    :param pointer: int
+    :param choices_made: int
+    :param input: int
+    :param result: int
+    :return:
+    """
+
+    if pointer < 0:
+        # index out of bounds
+        result.append(copy.deepcopy(choices_made))
+        # copy current choice to result array and backtrack to last choice
+        return
+
+    choices_made.append(input[pointer])
+    # add current element pointer is pointing at to the list of choices made
+    powerset_helper(pointer - 1, choices_made, input, result)
+
+    # now we can backtrack and remove last element we added to choices made
+    del choices_made[-1]
+    powerset_helper(pointer - 1, choices_made, input, result)
+
+
+
+
+
+
+
+
+
+
+
