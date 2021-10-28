68005092b1054cfc40650089465b814f2946a18a
Victoria Zavala <torizavala@aol.com>
2021-10-27 19:09:58 -0700

    3rd commit - both maxset and powerset finished

---
+++ Amount.py
@@ -0,0 +1,68 @@
+# Implement a backtracking algorithm
+# Given a collection of amount values (A) and a S sum (S), find all unique combinations in A where the amount
+# values sum up to S.
+# Each amount value may be used only once in the combination.
+# The solution set should not contain duplicate combinations.
+# Amounts will be positive numbers.
+# Example: A = [11,1,3,2,6,1,5]; S Sum = 8 Result = [3, 5]
+# [2, 6] [1, 2, 5] [1, 1, 6]
+
+# a) Describe a backtracking algorithm to solve this problem.
+
+# Since we need to find all possible results, and not best result we can use backtracking instead of dynamic
+# programming for this problem. To solve, we would need to iterate through each element in the array, starting from the
+# beginning of the array, storing as we go. When we see that the target value is less than the sum of the combination of
+# the choices, we backtrack to the previous selection and try a different path to find the solution.
+
+
+# b) implementation of solution
+
+def amount(A, S):
+    """
+     Given a collection of amount values (A) and a S sum (S), this function will find all unique combinations in A where
+    the amount values sum up to S.
+    """
+    A.sort()
+    result = []
+    combination = []
+    amount_helper(result, combination, A, 0, S)
+    return result
+
+
+def amount_helper(result, combination, A, start, S):
+    """
+    This function is a helper function for amount. It implements a backtracking algorithm to find a combination of
+    integers that will add up to S sum S
+    """
+    if S < 0:
+        return
+    # check to see if sum exceeded the target
+
+    if S == 0:
+        # we have found a result
+        result.append(list(combination))
+        return
+    # above are base cased needed to stop recursion
+
+    previous_num = -1
+    # make sure we don't have duplicates
+    length = len(A)
+    for i in range(start, length):
+        # iterate through array
+        # find result by making the choice of keeping each number in array
+        if A[i] != previous_num:
+            combination.append(A[i])
+            # store results in combination
+            amount_helper(result, combination, A, i + 1, S - A[i])  # we pass 'i + 1' and not 'i' as duplicates NOT allowed
+            previous_num = A[i]
+            # backtrack to remove last selection in the array
+            combination.pop()
+
+if __name__ == '__main__':
+    print(amount([11, 1, 3, 2, 6, 1, 5], 8))
+
+
+# c) Time-Complexity: Exponential Complexity - O(2^n)
+
+
+
